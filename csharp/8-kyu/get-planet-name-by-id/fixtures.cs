ccee4373cc7cbbd2bc31006d5a4afc99dc786586
MarcusDLG <delag010@gmail.com>
2020-06-17 13:32:20 -0400

    switch cases underway!

---
+++ csharp/8-kyu/get-planet-name-by-id/fixtures.cs
@@ -0,0 +1,15 @@
+namespace Solution {
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void Test()
+    {
+      Assert.AreEqual("Venus", Kata.GetPlanetName(2));
+      Assert.AreEqual("Jupiter", Kata.GetPlanetName(5));
+    }
+  }
+}
